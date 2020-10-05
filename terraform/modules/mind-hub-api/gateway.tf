1b96370e1d3a2622fab1f1c0319b719b1f74b373
Jonny Pillar <info@jonnypillar.co.uk>
2020-10-05 16:07:25 +0100

    aws_api_gateway_integration Fix

--- terraform/modules/mind-hub-api/gateway.tf
+++ terraform/modules/mind-hub-api/gateway.tf
@@ -81,12 +81,12 @@ resource "aws_api_gateway_method_response" "mind_hub_api_resource_post_method_re
 }
 
 resource "aws_api_gateway_integration" "mind_hub_api_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.mind_hub_api.id}"
-  resource_id             = "${aws_api_gateway_resource.mind_hub_api_resource.id}"
-  http_method             = "${aws_api_gateway_method.mind_hub_api_resource_post_method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.mind_hub_api.id
+  resource_id             = aws_api_gateway_resource.mind_hub_api_resource.id
+  http_method             = aws_api_gateway_method.mind_hub_api_resource_post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.mind_hub_api_graphql_api_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.mind_hub_api_graphql_api_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_integration_response" "mind_hub_api_resource_post_method_integration_response" {
@@ -116,7 +116,7 @@ resource "aws_lambda_permission" "mind_hub_api_graphql_api_lambda_permission" {
   function_name = "${aws_lambda_function.mind_hub_api_graphql_api_lambda.function_name}"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.mind_hub_api.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.mind_hub_api.execution_arn}/*/${aws_api_gateway_method.mind_hub_api_resource_post_method.http_method}${aws_api_gateway_resource.mind_hub_api_resource.path}"
 }
 
 resource "aws_api_gateway_deployment" "mind_hub_api_deploy" {

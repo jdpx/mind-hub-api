2b4858d77056cca4f5c3b9f657167baaeb9f3d95
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2020-10-21 16:09:56 +0100

    Updated API Gateway To Proxy Method (#25)

--- pkg/api/middleware.go
+++ pkg/api/middleware.go
@@ -20,7 +20,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "PUT", "OPTIONS", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", correlationIDHeader},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "x-correlation-id"},
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "x-correlation-id"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
--- terraform/modules/mind-hub-api/gateway.tf
+++ terraform/modules/mind-hub-api/gateway.tf
@@ -16,8 +16,16 @@ module "cors" {
   version = "0.3.1"
 
   api_id            = aws_api_gateway_rest_api.mind_hub_api.id
-  api_resource_id   = aws_api_gateway_resource.mind_hub_api_resource.id
+  api_resource_id   = aws_api_gateway_resource.mind_hub_proxy.id
   allow_credentials = true
+  allow_headers = [
+    "Authorization",
+    "Content-Type",
+    "X-Amz-Date",
+    "X-Amz-Security-Token",
+    "X-Api-Key",
+    "x-correlation-id",
+  ]
 }
 
 module authorizer {
@@ -26,7 +34,6 @@ module authorizer {
   authorizer_jwks_uri     = var.auth0_jwks_uri
   authorizer_token_issuer = var.auth0_token_issuer
 }
-
 
 resource "aws_api_gateway_rest_api" "mind_hub_api" {
   name = "mind_hub_api_${var.env}"
@@ -38,20 +45,42 @@ resource "aws_api_gateway_base_path_mapping" "base_path_mapping" {
   domain_name = "${aws_api_gateway_domain_name.mind_hub_api_domain.domain_name}"
 }
 
-
-resource "aws_api_gateway_resource" "mind_hub_api_resource" {
-  path_part   = "query"
-  parent_id   = "${aws_api_gateway_rest_api.mind_hub_api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.mind_hub_api.id}"
+resource "aws_api_gateway_resource" "mind_hub_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
+  parent_id   = aws_api_gateway_rest_api.mind_hub_api.root_resource_id # aws_api_gateway_resource.version.id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "mind_hub_api_resource_post_method" {
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
+  resource_id = aws_api_gateway_resource.mind_hub_proxy.id
+  http_method = "ANY"
+
+
   authorization = "CUSTOM"
   authorizer_id = aws_api_gateway_authorizer.authorizer.id
 
-  rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
-  resource_id = aws_api_gateway_resource.mind_hub_api_resource.id
-  http_method = "POST"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "mind_hub_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.mind_hub_api.id
+  resource_id             = aws_api_gateway_resource.mind_hub_proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.mind_hub_api_graphql_api_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "mind_hub_api_graphql_api_lambda_permission_proxy" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.mind_hub_api_graphql_api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.mind_hub_api.execution_arn}/*/${aws_api_gateway_method.proxy_method.http_method}${aws_api_gateway_resource.mind_hub_proxy.path}"
 }
 
 resource "aws_api_gateway_authorizer" "authorizer" {
@@ -61,67 +90,17 @@ resource "aws_api_gateway_authorizer" "authorizer" {
   rest_api_id            = aws_api_gateway_rest_api.mind_hub_api.id
 }
 
-resource "aws_api_gateway_method_response" "mind_hub_api_resource_post_method_response_200" {
-  rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
-  resource_id = aws_api_gateway_resource.mind_hub_api_resource.id
-  http_method = aws_api_gateway_method.mind_hub_api_resource_post_method.http_method
-  status_code = 200
-
-  /**
-   * This is where the configuration for CORS enabling starts.
-   * We need to enable those response parameters and in the 
-   * integration response we will map those to actual values
-   */
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = true,
-    "method.response.header.Access-Control-Allow-Methods"     = true,
-    "method.response.header.Access-Control-Allow-Origin"      = true,
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "mind_hub_api_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.mind_hub_api.id
-  resource_id             = aws_api_gateway_resource.mind_hub_api_resource.id
-  http_method             = aws_api_gateway_method.mind_hub_api_resource_post_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.mind_hub_api_graphql_api_lambda.invoke_arn
-}
-
-resource "aws_api_gateway_integration_response" "mind_hub_api_resource_post_method_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
-  resource_id = aws_api_gateway_resource.mind_hub_api_resource.id
-  http_method = aws_api_gateway_method.mind_hub_api_resource_post_method.http_method
-  status_code = aws_api_gateway_method_response.mind_hub_api_resource_post_method_response_200.status_code
-
-  depends_on = [aws_api_gateway_integration.mind_hub_api_integration]
-
-  /**
-   * This is second half of the CORS configuration.
-   * Here we give values to each of the header parameters to ALLOW 
-   * Cross-Origin requests from ALL hosts.
-   **/
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Correlation-Id'",
-    "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"      = "'*'",
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
-}
-
-resource "aws_lambda_permission" "mind_hub_api_graphql_api_lambda_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.mind_hub_api_graphql_api_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.mind_hub_api.execution_arn}/*/${aws_api_gateway_method.mind_hub_api_resource_post_method.http_method}${aws_api_gateway_resource.mind_hub_api_resource.path}"
-}
-
 resource "aws_api_gateway_deployment" "mind_hub_api_deploy" {
-  depends_on = [aws_api_gateway_integration.mind_hub_api_integration]
+  depends_on = [
+    aws_api_gateway_resource.mind_hub_proxy,
+    aws_api_gateway_method.proxy_method,
+    aws_api_gateway_integration.mind_hub_lambda_integration
+  ]
 
   rest_api_id = aws_api_gateway_rest_api.mind_hub_api.id
   stage_name  = "v1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }

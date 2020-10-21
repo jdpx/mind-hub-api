e6b601dc9b8940aa820b3acb6c0c23ed3efea8bc
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2020-10-21 14:47:15 +0100

    Added Request Logging (#24)
    * Added Request Logging
    
    * Lint Fixes
    
    * Update logger.go

---
+++ pkg/api/middleware.go
@@ -0,0 +1,58 @@
+package api
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jdpx/mind-hub-api/pkg/logging"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	correlationIDHeader = "X-Correlation-Id"
+)
+
+// CORSMiddleware ...
+func CORSMiddleware() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "OPTIONS", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", correlationIDHeader},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "x-correlation-id"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
+
+// RequestLoggerMiddleware ...
+func RequestLoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cID := c.Request.Header.Get(correlationIDHeader)
+
+		if cID == "" {
+			id, _ := uuid.NewUUID()
+			cID = id.String()
+		}
+
+		log := logging.New().WithFields(logrus.Fields{
+			logging.CorrelationIDKey: cID,
+		})
+
+		log.Info(fmt.Sprintf("Request %s starting", c.Request.URL.Path))
+
+		t := time.Now()
+
+		c.Next()
+
+		latency := time.Since(t).Milliseconds()
+		status := c.Writer.Status()
+
+		log.WithFields(logrus.Fields{
+			logging.RequestDurationKey: latency,
+			logging.HTTPStatusKey:      status,
+		}).Info(fmt.Sprintf("Request %s completed ", c.Request.URL.Path))
+	}
+}
--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,16 +1,14 @@
 package api
 
 import (
-	"log"
-	"time"
-
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	ginHelper "github.com/jdpx/mind-hub-api/pkg/gin"
 	"github.com/jdpx/mind-hub-api/pkg/graphcms"
 	"github.com/jdpx/mind-hub-api/pkg/graphql/graph"
 	"github.com/jdpx/mind-hub-api/pkg/graphql/graph/generated"
+	"github.com/jdpx/mind-hub-api/pkg/logging"
 	"github.com/machinebox/graphql"
 )
 
@@ -21,29 +19,24 @@ type Config struct {
 
 // NewRouter ...
 func NewRouter(config *Config) *gin.Engine {
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Gin cold start")
+	log := logging.New()
+
+	log.Info("Gin cold start")
 	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "PUT", "OPTIONS", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	r.Use(CORSMiddleware())
+	r.Use(RequestLoggerMiddleware())
+	r.Use(ginHelper.RequestContextToContextMiddleware())
 
 	// Setting up Gin
 	r.POST("v1/query", graphqlHandler(config))
 	r.GET("v1/playground", playgroundHandler())
 	r.GET("v1/ping", func(c *gin.Context) {
-		log.Println("Handler!!")
+		log.Info("Handler!!")
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-
 	return r
 }
 

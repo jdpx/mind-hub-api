f0e000c70a75a6ae9edf5f7be45b45a25c43cacc
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2021-02-07 19:29:48 +0000

    Updated DynamDB Tables (#62)

--- terraform/modules/mind-hub-api/dynamodb.tf
+++ terraform/modules/mind-hub-api/dynamodb.tf
@@ -1,88 +1,18 @@
-resource "aws_dynamodb_table" "mind_hub_api_course_progress" {
-  name              = "course_progress"
+resource "aws_dynamodb_table" "mind_hub_api_user_table" {
+  name              = "user"
   read_capacity     = 5
   write_capacity    = 5
-  hash_key          = "courseID"
-  range_key         = "userID"
+  hash_key          = "PK"
+  range_key         = "SK"
   stream_enabled    = false
 
   attribute {
-    name = "courseID"
+    name = "PK"
     type = "S"
   }
 
   attribute {
-    name = "userID"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "mind_hub_api_course_note" {
-  name              = "course_note"
-  read_capacity     = 5
-  write_capacity    = 5
-  hash_key          = "courseID"
-  range_key         = "userID"
-  stream_enabled    = false
-
-  attribute {
-    name = "courseID"
-    type = "S"
-  }
-
-  attribute {
-    name = "userID"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "mind_hub_api_step_progress" {
-  name              = "step_progress"
-  read_capacity     = 5
-  write_capacity    = 5
-  hash_key          = "stepID"
-  range_key         = "userID"
-  stream_enabled    = false
-
-  attribute {
-    name = "stepID"
-    type = "S"
-  }
-
-  attribute {
-    name = "userID"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "mind_hub_api_step_note" {
-  name              = "step_note"
-  read_capacity     = 5
-  write_capacity    = 5
-  hash_key          = "stepID"
-  range_key         = "userID"
-  stream_enabled    = false
-
-  attribute {
-    name = "stepID"
-    type = "S"
-  }
-
-  attribute {
-    name = "userID"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "mind_hub_api_timemap" {
-  name              = "timemap"
-  read_capacity     = 5
-  write_capacity    = 5
-  hash_key          = "userID"
-  stream_enabled    = false
-
-  attribute {
-    name = "userID"
+    name = "SK"
     type = "S"
   }
 }

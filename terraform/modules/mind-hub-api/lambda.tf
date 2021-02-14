942b5bd2a0cae58c6e7e1fcaad963a1aa29ebb6d
Jonny Pillar <info@jonnypillar.co.uk>
2021-02-14 21:41:15 +0000

    Add Lambda With Artefact Version ID

--- terraform/modules/mind-hub-api/lambda.tf
+++ terraform/modules/mind-hub-api/lambda.tf
@@ -3,9 +3,10 @@ resource "aws_lambda_function" "mind_hub_api_graphql_api_lambda" {
   function_name = "mind_hub_api_graphql_api_${var.env}"
   description   = "Lambda that contains the GraphQL API"
 
-  s3_bucket = data.aws_s3_bucket.mind_hub_api_pipeline_artifact_bucket.bucket
-  s3_key    = "graphql.zip"
-  role      = aws_iam_role.mind_hub_api_graphql_api_role.arn
+  s3_bucket         = data.aws_s3_bucket.mind_hub_api_pipeline_artifact_bucket.bucket
+  s3_key            = "graphql.zip"
+  s3_object_version = data.aws_s3_bucket_object.mind_hub_api_pipeline_artifact_bucket_object.version_id
+  role              = aws_iam_role.mind_hub_api_graphql_api_role.arn
 
   handler = "lambda"
   runtime = "go1.x"
--- terraform/modules/mind-hub-api/s3.tf
+++ terraform/modules/mind-hub-api/s3.tf
@@ -1,18 +1,8 @@
-# resource "aws_s3_bucket" "mind_hub_api_pipeline_artifact_bucket" {
-#   bucket = "mind-hub-api-artifacts-${var.env}"
-#   acl    = "private"
-
-#   lifecycle_rule {
-#         id      = "quarterly_retention"
-#         prefix  = "/"
-#         enabled = true
-
-#         expiration {
-#             days = 7
-#         }
-#     }
-# }
-
 data "aws_s3_bucket" "mind_hub_api_pipeline_artifact_bucket" {
   bucket = "mind-hub-api-pipeline-artifacts"
+}
+
+data "aws_s3_bucket_object" "mind_hub_api_pipeline_artifact_bucket_object" {
+  bucket = "mind-hub-api-pipeline-artifacts"
+  key    = "graphql.zip"
 }

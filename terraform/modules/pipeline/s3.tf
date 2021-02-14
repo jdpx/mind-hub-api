5cd18b445d195b43c623938e5bf0bcd55eefa6e8
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2021-02-14 21:35:31 +0000

    Added Artefact Bucket Version (#69)

--- terraform/modules/pipeline/s3.tf
+++ terraform/modules/pipeline/s3.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "mind_hub_api_pipeline_artifact_bucket" {
   bucket = "mind-hub-api-pipeline-artifacts"
   acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+
   lifecycle_rule {
     id      = "quarterly_retention"
     prefix  = "/"
